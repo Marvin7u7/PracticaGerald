6fffd159bbd056f542f3175a7bad87bb39924677
Marlie Ramirez <marlie.ramirez12169@est.uca.edu.ni>
2020-11-29 09:15:15 -0600

    Modificado Marlie

--- PracticaGreald/src/practicagreald/PracticaGreald.java
+++ PracticaGreald/src/practicagreald/PracticaGreald.java
@@ -16,6 +16,7 @@ public class PracticaGreald {
      */
     public static void main(String[] args) {
         System.out.println("Hola mundo");
+        System.out.println("Aqui toy!");
     }
     
 }
